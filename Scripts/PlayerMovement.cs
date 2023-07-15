3d581211a95806939d41788fabf021a90fa84a5e
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-16 04:10:00 +0430

    Add movement functionality of the player
    In the past, we usually used this.transform.Translate() for
    change of position. However that requires us to not only handle
    this aspect but also the collision aspect of things. But we already
    have a rigidBody2D component attached to out object. Therefore we should
    use that this component's velocity field in order to facilitate this
    process for us.

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    private void Movement()
+    {
+        float valueOfDisplacement = Input.GetAxis("Horizontal");
+        valueOfDisplacement *= 10f;
+        rigidBody.velocity = new Vector2(valueOfDisplacement, 0);
+            
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = this.transform.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+}
