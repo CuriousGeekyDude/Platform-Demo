4353a08f8a289753ebfd6c722485fb1818f0cb3e
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-07-18 15:43:37 +0430

    Position the spawned platforms relative to player

--- Scripts/SceneController.cs
+++ Scripts/SceneController.cs
@@ -7,6 +7,16 @@ public class SceneController : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject platformBluePrint;
     private GameObject[] clonedPlatforms = new GameObject[2];
+
+
+    private void PositionPlatforms()
+    {
+        float positionPlayerY = player.transform.position.y;
+        float positionPlayerX = player.transform.position.x;
+        clonedPlatforms[0].transform.position = new Vector3(positionPlayerX+Random.Range(-5f, 5f), positionPlayerY-5f, player.transform.position.z);
+        clonedPlatforms[1].transform.position = new Vector3(positionPlayerX+Random.Range(-5f, 5f), positionPlayerY+5f, player.transform.position.z);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
